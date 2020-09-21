6aea76df7500cc6ead4779562cc21d24845342cb
Boris Andrzhiievskyi <borees2@gmail.com>
2020-09-21 14:13:54 +0300

    fix model

--- models/wobjectSubscriptionModel.js
+++ models/wobjectSubscriptionModel.js
@@ -24,7 +24,7 @@ exports.getBellFollowers = async ({ following }) => {
   try {
     const result = await WobjectSubscriptions.find({ following, bell: true }).select('follower')
       .lean();
-    return { wobjFollowers: result.map((el) => el.follower) };
+    return { users: result.map((el) => el.follower) };
   } catch (error) {
     return { error };
   }
