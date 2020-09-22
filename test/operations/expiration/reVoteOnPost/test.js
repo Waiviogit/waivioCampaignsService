58cedcf617fa1ac638ae92c9f63ffd87d558fce1
olegtsigulov <olegtsigulov@gmail.com>
2020-09-22 10:43:02 +0300

    add tests for voteParser

--- test/operations/expiration/reVoteOnPost/test.js
+++ test/operations/expiration/reVoteOnPost/test.js
@@ -1,5 +1,35 @@
-const { expect, revoteOnPost } = require('test/testHelper');
+const {
+  expect, revoteOnPost, dropDatabase, sinon, faker, steemHelper,
+} = require('test/testHelper');
 
 describe('On reVoteOnPost', async () => {
+  beforeEach(async () => {
+    await dropDatabase();
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  describe('without upvotes', async () => {
+    beforeEach(async () => {
+      sinon.spy(steemHelper, 'getPostInfo');
+    });
+    it('should not call method to get post info without upvotes', async () => {
+      await revoteOnPost({ author: faker.name.firstName(), permlink: faker.random.string(10) });
+      expect(steemHelper.getPostInfo.notCalled).to.be.true;
+    });
+  });
+  describe('with post older then 7 days', async () => {
+    beforeEach(async () => {
+      sinon.stub(steemHelper, 'getPostInfo').returns(Promise.resolve());
 
+    });
+  });
+  describe('without downvotes on post', async () => {
+    beforeEach(async () => {
+    });
+  });
+  describe('with downvotes and pending payout 0', async () => {
+    beforeEach(async () => {
+    });
+  });
 });
