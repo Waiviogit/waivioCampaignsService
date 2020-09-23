13210da4494fd19e207260a732b5113b5e432bc6
olegtsigulov <olegcigulev@gmail.com>
2020-09-23 15:14:44 +0300

    add sentry logging

--- enviroment.js
+++ enviroment.js
@@ -55,7 +55,6 @@ module.exports = function (app, express) {
   app.use((err, req, res, next) => {
     // The error id is attached to `res.sentry` to be returned
     // and optionally displayed to the user for support.
-    res.statusCode = err.status || 500;
-    res.end(`${res.sentry}\n`);
+    res.status(err.status || 500).json({ message: err.message || 'Internal server error' });
   });
 };
