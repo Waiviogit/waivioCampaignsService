d108bc1ee658c5472d3dad1da7ea6b0efa7f1b43
olegtsigulov <olegcigulev@gmail.com>
2020-09-23 08:35:59 +0300

    fix vote parser

--- parsers/voteParser.js
+++ parsers/voteParser.js
@@ -6,16 +6,13 @@ const { campaignModel } = require('models');
 const { redisSetter, redisGetter } = require('utilities/redis');
 
 exports.parse = async (votes) => {
-  let names = [];
-  if (!votes.length) return;
-  const { result: campaigns } = await campaignModel.aggregate(campaignPipeline());
-  if (campaigns && campaigns.length) {
-    names = _.uniq(_.flattenDeep(_.concat(campaigns[0].matchBots, campaigns[0].guideNames)));
-    if (!names && !names.length) return;
-  }
   await Promise.all(votes.map(async (vote) => {
+    const { result: campaign } = await campaignModel.findOne({
+      $or: [{ guideName: vote.voter }, { match_bots: vote.voter }],
+      payments: { $elemMatch: { postPermlink: vote.permlink, rootAuthor: vote.author } },
+    });
     /** If voter not match-bot and weight <0 parse downvotes for recount vote */
-    if (!_.includes(names, vote.voter) && vote.weight < 0) {
+    if (!campaign && vote.weight < 0) {
       const { result } = await campaignModel.findOne({
         payments: { $elemMatch: { postPermlink: vote.permlink, rootAuthor: vote.author } },
       });
@@ -30,31 +27,8 @@ exports.parse = async (votes) => {
       if (ttlTime < 0) return;
       await redisSetter.setSimpleTtl(`${DOWNVOTE_ON_REVIEW}|${vote.author}|${vote.permlink}`, ttlTime);
       /** If voter match-bot - parse manual votes */
-    } else {
-      const { result } = await campaignModel.findOne({
-        $or: [{ guideName: vote.voter }, { match_bots: vote.voter }],
-        payments: { $elemMatch: { postPermlink: vote.permlink, rootAuthor: vote.author } },
-      });
-      if (!result) return;
+    } else if (campaign) {
       await redisSetter.setSimpleTtl(`${MATCH_BOT_VOTE}|${vote.author}|${vote.permlink}|${vote.voter}`, 20);
     }
   }));
 };
-
-const campaignPipeline = () => [
-  { $match: { users: { $elemMatch: { status: 'completed' } } } },
-  {
-    $group: {
-      _id: null,
-      guideNames: { $push: '$guideName' },
-      matchBots: { $push: '$match_bots' },
-    },
-  },
-];
-
-// (async () => {
-//   await this.parse([{
-//     voter: 'mack-bot', author: 'vanguide', permlink: 'review-kisamos-greek-taverna-greek-salad', weight: -1000,
-//   }]);
-//   console.log();
-// })();
