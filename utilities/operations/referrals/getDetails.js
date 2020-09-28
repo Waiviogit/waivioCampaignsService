f19e1c2f2d332604df7d1c0aa82ad5f81c4f4afb
olegtsigulov <olegcigulev@gmail.com>
2020-09-28 18:02:43 +0300

    new app schema

--- utilities/operations/referrals/getDetails.js
+++ utilities/operations/referrals/getDetails.js
@@ -5,20 +5,20 @@ const { appModel } = require('models');
 module.exports = async (appName) => {
   const { error, result } = await appModel.findOne(appName);
   if (error || !result) return { error: error || { status: 404, message: 'App not found!' } };
-
+  const campaignPercent = _.get(result, 'app_commissions.campaigns_percent', 0.3);
   const rewardsReferral = _.find(result.referralsData || [],
     { type: REFERRAL_TYPES.REVIEWS });
 
-  const indexServerPercent = _.round((1 - result.app_commissions.campaigns_percent)
-      * result.app_commissions.index_percent * 100, 5);
+  const indexServerPercent = _.round((1 - campaignPercent)
+      * _.get(result, 'app_commissions.index_percent', 0.2) * 100, 5);
 
-  const referralServerPercent = _.round((100 - (result.app_commissions.campaigns_percent * 100)
+  const referralServerPercent = _.round((100 - (campaignPercent * 100)
       - indexServerPercent), 5);
 
   return {
     result: {
       referralDuration: _.get(rewardsReferral, 'duration', 90),
-      campaignServerPercent: result.app_commissions.campaigns_percent * 100,
+      campaignServerPercent: campaignPercent * 100,
       indexServerPercent,
       referralServerPercent,
       suspendedTimer: SUSPENDED_DAYS,
