18220893590821daab42f7923609ad5b2f251d44
olegtsigulov <olegcigulev@gmail.com>
2020-09-30 11:08:12 +0300

    add env to sentry

--- utilities/operations/referrals/checkBlackList.js
+++ utilities/operations/referrals/checkBlackList.js
@@ -5,5 +5,5 @@ module.exports = async (params) => {
   const { result } = await appModel.findOne(params.host);
   if (!result) return { error: { status: 404, message: 'App not found!' } };
 
-  return { isBlacklisted: _.contains(result.black_list_users || [], params.userName) };
+  return { isBlacklisted: _.includes(result.black_list_users || [], params.userName) };
 };
