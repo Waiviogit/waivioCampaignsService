200488ed322b72e81d1fdd0f4577c319b47cae92
Boris Andrzhiievskyi <borees2@gmail.com>
2021-12-15 14:09:41 +0200

    Git_3326 add filter pools < 1000 volume

---
+++ utilities/hiveEngine/market.js
@@ -0,0 +1,10 @@
+const engineQuery = require('utilities/hiveEngine/engineQuery');
+
+exports.getMarketMetrics = async ({ query }) => engineQuery({
+  params: {
+    contract: 'market',
+    table: 'metrics',
+    query,
+    indexes: '',
+  },
+});
