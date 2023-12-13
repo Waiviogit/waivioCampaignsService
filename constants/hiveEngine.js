0ffa62d90675f06225700ef9ee6f800877de6550
fesmofet <borees2@gmail.com>
2023-12-13 11:47:34 +0200

    engine_nodes

--- constants/hiveEngine.js
+++ constants/hiveEngine.js
@@ -64,10 +64,8 @@ exports.VOTE_REGENERATION_DAYS = 5;
 exports.DOWNVOTE_REGENERATION_DAYS = 5;
 
 exports.HIVE_ENGINE_NODES = [
-  // 'https://api.hive-engine.com/rpc', // Germany
-//  'https://api2.hive-engine.com/rpc', // Finland
+  'https://engine.waivio.com',
   'https://herpc.dtools.dev',
-  'https://engine.rishipanthee.com',
   'https://api2.hive-engine.com/rpc',
   'https://api.primersion.com',
   'https://herpc.kanibot.com',
