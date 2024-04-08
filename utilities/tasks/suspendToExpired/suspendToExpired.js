bf5ba1a295dc491ed4b0496115aaeaa7718687e3
fesmofet <borees2@gmail.com>
2024-04-08 11:10:54 +0300

    suspend to expired task

--- utilities/tasks/suspendToExpired/suspendToExpired.js
+++ utilities/tasks/suspendToExpired/suspendToExpired.js
@@ -1,22 +1,10 @@
 const { CAMPAIGN_STATUSES } = require('constants/constants');
 const { CampaignV2 } = require('database').models;
-const moment = require('moment');
-const _ = require('lodash');
 
 const getStatusAfterSuspend = (campaign) => {
   if (campaign.deactivationPermlink) return CAMPAIGN_STATUSES.INACTIVE;
   if (campaign.expiredAt < new Date()) return CAMPAIGN_STATUSES.EXPIRED;
-  if (campaign.activationPermlink) {
-    const completedUsers = _.filter(
-      campaign.users,
-      (user) => user.createdAt > moment.utc().startOf('month').toDate(),
-    );
-    return campaign.budget - campaign.reward * completedUsers.length
-    > campaign.reward
-      ? CAMPAIGN_STATUSES.SUSPENDED
-      : CAMPAIGN_STATUSES.REACHED_LIMIT;
-  }
-  return CAMPAIGN_STATUSES.PENDING;
+  return CAMPAIGN_STATUSES.SUSPENDED;
 };
 
 module.exports = async () => {
