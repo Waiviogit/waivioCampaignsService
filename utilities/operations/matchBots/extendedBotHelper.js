0de9ec29cf71c683fd795f2319e5f20be4853159
fesmofet <borees2@gmail.com>
2025-10-16 14:25:04 +0300

    last moment vote

--- utilities/operations/matchBots/extendedBotHelper.js
+++ utilities/operations/matchBots/extendedBotHelper.js
@@ -1,10 +1,10 @@
-const { redisSetter, redisGetter } = require('utilities/redis');
 const crypto = require('node:crypto');
+const moment = require('moment');
+const { redisSetter, redisGetter } = require('utilities/redis');
 const { getPostInfo } = require('../../hiveApi/hiveOperations');
 const { voteExtendedMatchBots } = require('../../helpers/matchBotHelper');
-
 // need to add our validation time?
-const SUPPOSED_DELAY_SEC = 9 + 3;
+const SUPPOSED_DELAY_SEC = 6 + 1;
 const LAST_MOMENT_VOTE_KEY = 'lastMomentVote';
 
 const getsSecondsToCashout = async (author, permlink) => {
@@ -13,22 +13,21 @@ const getsSecondsToCashout = async (author, permlink) => {
   // eslint-disable-next-line camelcase
   const { cashout_time, created } = post;
 
-  let cashoutDate;
+  let cashoutMoment;
   // eslint-disable-next-line camelcase
   if (cashout_time) {
-    cashoutDate = new Date(cashout_time);
+    cashoutMoment = moment.utc(cashout_time);
   } else {
-    // Fallback: created + 7 days
-    cashoutDate = new Date(created);
-    cashoutDate.setDate(cashoutDate.getDate() + 7);
+    cashoutMoment = moment.utc(created).add(7, 'days');
   }
 
-  const now = new Date();
-  const ttlMs = cashoutDate.getTime() - now.getTime();
+  const now = moment.utc();
+  const ttlMs = cashoutMoment.valueOf() - now.valueOf();
   const ttlSeconds = Math.floor(ttlMs / 1000);
 
   return ttlSeconds > 0 ? ttlSeconds : 0;
 };
+
 const setExpireLastMomentVote = async (voteData) => {
   const { author, permlink } = voteData;
   const secondsToCashout = await getsSecondsToCashout(author, permlink);
@@ -36,13 +35,17 @@ const setExpireLastMomentVote = async (voteData) => {
   const ttl = secondsToCashout - SUPPOSED_DELAY_SEC;
   if (ttl <= (1)) return;
   const id = crypto.randomUUID();
+
   await redisSetter.setSimpleTtl(`expire:${LAST_MOMENT_VOTE_KEY}|${id}`, ttl);
-  await redisSetter.saveTTL(`${LAST_MOMENT_VOTE_KEY}:${id}`, ttl, JSON.stringify(voteData));
+  await redisSetter.saveTTL(`${LAST_MOMENT_VOTE_KEY}:${id}`, ttl + 30, JSON.stringify(voteData));
 };
 
 const lastMomentVote = async (id) => {
   const { result } = await redisGetter.getTTLData(`${LAST_MOMENT_VOTE_KEY}:${id}`);
-  if (!result) return;
+  if (!result) {
+    console.log(`cant find lastMomentVote data ${id}`);
+    return;
+  }
   await voteExtendedMatchBots(result);
 };
 
